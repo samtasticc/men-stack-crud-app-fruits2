3019304a90d90986ccd13a70f77ac83c606f3bc7
samtasticc <samantha.henesy@gmail.com>
2024-09-18 21:21:47 -0400

    created views, index.ejs, and connected mongoose

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // Here is where we import modules
+require('dotenv').config() // this imports and converts, it has to be the first line of code.
 // We begin by loading Express
 const express = require("express");
-
+const mongoose = require('mongoose')
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+app.get('/', (req, res) => {
+    res.render('index.ejs') // this tells the server to display the HTML that is on the index.ejs page
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
